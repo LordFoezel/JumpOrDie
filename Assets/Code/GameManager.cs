4133ae29ffcb967f6ec10f60d8ab188300313bd2
LordFoezel <simon.bertof@bluewin.ch>
2023-11-24 16:19:53 +0100

    fix

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -2,26 +2,18 @@ using System.Collections.Generic;
 
 public static class GameManager
 {
-    private static UtilEnum.GameState actualGameState;
-    private static int actualGameLevel = 0;
-    private static PlayerBase player;
-    private static Dictionary<int, TrapBase> traps = new Dictionary<int, TrapBase>();
-    private static Dictionary<int, PotionBase> potions = new Dictionary<int, PotionBase>();
-    private static Dictionary<int, CoinBase> coins = new Dictionary<int, CoinBase>();
-
-
-    public static UtilEnum.GameState ActualGameState { get => actualGameState; set => actualGameState = value; }
-    public static int ActualGameLevel { get => actualGameLevel; set => actualGameLevel = value; }
-    public static PlayerBase Player { get => player; set => player = value; }
-    public static Dictionary<int, TrapBase> Traps { get => traps; set => traps = value; }
-    public static Dictionary<int, PotionBase> Potions { get => potions; set => potions = value; }
-    public static Dictionary<int, CoinBase> Coins { get => coins; set => coins = value; }
+    public static UtilEnum.GameState ActualGameState { get; set; }
+    public static int ActualGameLevel { get; set; } = 0;
+    public static PlayerBase Player { get; set; } = null;
+    public static Dictionary<int, TrapBase> Traps { get; set; } = new Dictionary<int, TrapBase>();
+    public static Dictionary<int, PotionBase> Potions { get; set; } = new Dictionary<int, PotionBase>();
+    public static Dictionary<int, CoinBase> Coins { get; set; } = new Dictionary<int, CoinBase>();
 
     public static void ClearAll()
     {
-        if (player != null)
+        if (Player != null)
         {
-            player.Remove();
+            Player.Remove();
             Player = null;
         }
         foreach (KeyValuePair<int, TrapBase> trap in Traps)
